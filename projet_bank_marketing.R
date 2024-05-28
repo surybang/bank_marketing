6237dc783662f0720b230e5fee01daf00a4bca4f
surybang <44811505+surybang@users.noreply.github.com>
2024-05-28 07:14:53 +0200

    nouveau XGBOOST

--- projet_bank_marketing.R
+++ projet_bank_marketing.R
@@ -18,7 +18,7 @@ library(randomForest)
 data <- read.csv('C:/Users/hosfa/Downloads/bank+marketing/bank/bank-full.csv', stringsAsFactors = T,sep=';')
 names(data)
 #data <- subset(data,select=-c(pdays, nr.employed)) # data leakage
-data <- subset(data,select=-pdays) # data leakage
+data <- subset(data,select= -duration) # data leakage
 prop.table(table(data$y))
 summary(data)
 
@@ -26,11 +26,11 @@ summary(data)
 
 #data$y <- as.factor(ifelse(data$y=='yes',1,0))
 var_quali <- c('job','marital','education', 'default','housing','loan','contact','poutcome','month')
-var_num <- c('age','balance','day','campaign','previous','duration')
+var_num <- c('age','balance','day','campaign','previous','pdays')
 
 
 # Univariée
-par(mfrow=c(3,3))
+par(mfrow=c(4,2))
 for (variable in var_num) {
   plot(density(data[[variable]], na.rm = TRUE), main = paste("Densité de", variable), xlab = variable)
 }
@@ -39,16 +39,16 @@ for (variable in var_num) {
 # Bivariée quali
 GGally::ggbivariate(data, "y",
                     explanatory = var_quali) 
-
+par(mfrow=c(4,2))
 # bivariée quanti
 for (var in var_num) {
-  boxplot(data[[var]] ~ data$y, main = var, xlab = "Outcome", ylab = var)
+  boxplot(data[[var]] ~ data$y, main = paste("Analyse bivariée de", var), xlab =variable)
 }
 
 
 #corrélations 
 matrice_cor <- cor(data[, var_num])
-
+par(mfrow=c(1,1))
 # Création du graphique de la matrice de corrélation
 corrplot(matrice_cor, method = "color", type = "upper", 
          tl.col = "black", tl.srt = 45, addCoef.col = "black")
@@ -59,29 +59,54 @@ train <- data[index, ]
 test <- data[-index, ]
 
 cat("Proportion de la variable cible sur le jeu d'apprentissage")
+table(train$y)
 prop.table(table(train$y))
 
 cat("\nProportion de la variable cible sur le jeu de test")
+table(test$y)
 prop.table(table(test$y))
 
-#Regression logistique 
-LR <- glm(y~., data= train, family = binomial)
+
+
+# ajouter des poids 
+class_weights <- ifelse(train$y == 'yes',
+                        1 / table(train$y)[1],
+                        1 / table(train$y)[2])
+
+# Entraîner le modèle de régression logistique
+LR <- glm(y ~ . -month -pdays, data = train, family = binomial, weights = class_weights)
 summary(LR)
 
-
-#proba
+# Calculer les probabilités prédictives sur l'ensemble de test
 proba <- predict(LR, newdata = test, type = "response")
-#pred
-pred <- factor(ifelse(proba > 0.5, "positive","negative"))
-print(table(pred))
-#mc
-mc <- table(test$y,pred)
-mc
-#tx erreur
-err <- 1.0-sum(diag(mc))/sum(mc)
-err*100
 
-table(data$job)
+# Fonction pour calculer la sensibilité pour un seuil donné
+calculate_sensitivity <- function(threshold, proba, actual) {
+  pred <- factor(ifelse(proba > threshold, "positive", "negative"), levels = c("negative", "positive"))
+  mc <- table(actual, pred)
+  sensitivity <- mc[2, 2] / sum(mc[2, ])
+  return(sensitivity)
+}
+
+# Tester différents seuils pour trouver celui qui maximise la sensibilité
+thresholds <- seq(0.1, 0.9, by = 0.01)
+sensitivities <- sapply(thresholds, calculate_sensitivity, proba = proba, actual = test$y)
+
+# Trouver le seuil optimal
+optimal_threshold <- thresholds[which.max(sensitivities)]
+max_sensitivity <- max(sensitivities)
+
+# Afficher le seuil optimal et la sensibilité correspondante
+cat("Seuil optimal:", optimal_threshold, "\n")
+cat("Sensibilité maximale:", max_sensitivity, "\n")
+
+# Utiliser le seuil optimal pour faire des prédictions
+optimal_pred <- factor(ifelse(proba > optimal_threshold, "positive", "negative"), levels = c("negative", "positive"))
+print(table(optimal_pred))
+
+# Matrice de confusion avec le seuil optimal
+optimal_mc <- table(test$y, optimal_pred)
+optimal_mc
 
 # evaluation des variables 
 res <- summary(LR)
@@ -101,21 +126,22 @@ control <- trainControl(method = "cv",
 # parameter grid for XGBoost
 parameterGrid <-  expand.grid(eta = c(0.001, 0.01, 0.1), # shrinkage (learning rate)
                               colsample_bytree = c(0.3, 0.5, 0.7), # subsample ration of columns
-                              max_depth = c(1, 2, 3, 4), # max tree depth. model complexity
-                              nrounds = c(30, 50, 70, 100), # boosting iterations
+                              max_depth = c( 2, 3 ), # max tree depth. model complexity
+                              nrounds = c(50, 100), # boosting iterations
                               gamma = 0.1, # minimum loss reduction
-                              subsample = 0.8, # ratio of the training instances
+                              subsample = c(0.6, 0.8), # ratio of the training instances
                               min_child_weight = c(2,3,4)) # minimum sum of instance weight
 
 # parameter grid for random forest
 # mtry = the number of features to use to build each tree
-rfGrid <- expand.grid(mtry = seq(from = 4, to = 20, by = 4))
+rfGrid <- expand.grid(mtry = seq(from = 4, to = 8, by = 1))
 
-model_xgb <- train(y~.,
+model_xgb <- train(y~ . -month -pdays,
                    data = train,
                    method = "xgbTree",
                    trControl = control,
-                   tuneGrid = parameterGrid)
+                   tuneGrid = parameterGrid,
+                   verbose=F)
 print(model_xgb)
 #plot(model_xgb)
 
